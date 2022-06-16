64bfea299d527d26e932732b7d645a3f0e3a3d35
Cesar Feliciano <cesarfeliciano1@aol.com>
2022-06-16 00:13:39 -0400

    fixed destructuring bug to display items and made a function to handle change and filter them names

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Coins from './Components/Coins/coins';
 
 const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
 
 function App() {
 
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {axios.get(url)
   .then(res => {
@@ -15,15 +17,33 @@ function App() {
     console.log(res.data);
   }).catch(err => alert(err))}, []);
     
+const handleChange = (e) => {
+  setSearch(e.target.value);
+}
+
+const filteredCoins = coins.filter(coin =>
+  coin.name.toLowerCase().includes(search.toLowerCase())
+  )
 
   return (
     <div className="coin-app">
        <div className='coin-search'>
        <h1 className='coin-text'>Search for a currency</h1>
        <form>
-          <input className='coin-input' type='text' placeholder='Search'></input>
+          <input className='coin-input' type='text' placeholder='Search' onChange={handleChange}></input>
        </form>
       </div>
+      {filteredCoins.map(coin => {
+        return (
+        <Coins 
+          key={coin.id} 
+          name={coin.name} 
+          image={coin.image} 
+          symbol={coin.symbol} 
+          price={coin.current_price} 
+          volume={coin.total_volume}/>
+        );
+      })}
     </div>
   );
 }
