2b2f2a7e47e28b3dd1dc7cd857c799ce0d2561a0
Cesar Feliciano <cesarfeliciano1@aol.com>
2022-06-23 14:50:22 -0400

    added necessary information for the coinstats page

--- src/Pages/coinStats.js
+++ src/Pages/coinStats.js
@@ -1,6 +1,32 @@
 const coinStats = ({}) => {
     return (
-        <div></div>
+        <div className="coinstats-container">
+            <div className="coinstats-name__container">
+                <h1 className="coinstats-title"></h1>
+                <p className="coinstats-symbol"></p>
+            </div>
+            <div className="coinstats-data__container">
+                <p className="coinstats-price"></p>
+                <p className="coinstats-change"></p>
+                <div className="coinstats-data__info">
+                    <p className="coinstats-marketcap"></p>
+                    <p className="coinstats-volume"></p>
+                    <p className="coinstats-circulating"></p>
+                    <p className="coinstats-total"></p>
+                </div>
+            </div>
+                <div className="coinstats-price-stats__container">
+                    <h2 className="coinstats-price-stats__title">Price Stats</h2>
+                    <p className="coinstats-price"></p>
+                    <p className="coinstats-change-high-low"></p>
+                    <p className="coinstats-volume"></p>
+                    <p className="coinstats-marketcap-rank"></p>
+                    <p className="coinstats-marketcap"></p>
+                    <p className="coinstats-alltimehigh"></p>
+                    <p className="coinstats-alltimelow"></p>
+                </div>
+        
+        </div>
     )
 }
 
